c6965528c9561ee00adb204e096259f44db03c19
Alex Beregszaszi <alex@rtfs.hu>
2019-07-15 02:36:55 +0100

    Update to eip1962 version 0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 use evmc_declare::evmc_declare_vm;
 use evmc_vm::*;
 
-use eth_pairings::{PrecompileAPI, API};
-
 // FIXME: use 'precompile'
 #[evmc_declare_vm("EIP1962", "evm", "0.1.0")]
 pub struct EIP1962;
@@ -24,14 +22,7 @@ impl EvmcVm for EIP1962 {
 
         let input = msg.input().unwrap();
 
-        let result = match input[0] {
-            0 => API::add_points(&input[1..]),
-            1 => API::mul_point(&input[1..]),
-            2 => API::multiexp(&input[1..]),
-            // FIXME: support pairing
-            _ => Err(()),
-        };
-
+        let result = eth_pairings::public_interface::API::run(&input);
         if result.is_err() {
             return ExecutionResult::failure();
         }
